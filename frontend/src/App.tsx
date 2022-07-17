85f710dcc33ebc766345d2274daf7a0bc52c403b
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2022-07-16 22:40:54 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,17 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import NotificationButton from './components/NotificationButton'
-import SalesCard from './components/SalesCard'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './components/Header';
+import SalesCard from './components/SalesCard';
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
           <div className="dsmeta-container">
             <SalesCard />
-
-
           </div>
         </section>
       </main>
@@ -20,4 +19,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
