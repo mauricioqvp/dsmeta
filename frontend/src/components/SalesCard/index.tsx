e5f8f561420526d1e01abe82f5c48c382ee11b88
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2022-07-16 22:09:08 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -20,12 +20,15 @@ function SalesCard() {
 
     useEffect(() => {
 
-        axios.get(`${BASE_URL}/sales`)
+        const dmin = minDate.toISOString().slice(0, 10);
+        const dmax = maxDate.toISOString().slice(0, 10);
+
+        axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
             .then(response => {
                 setSales(response.data.content);
             });
         console.log(sales);
-    }, []);
+    }, [minDate, maxDate]);
 
     return (
         <div className="dsmeta-card">
